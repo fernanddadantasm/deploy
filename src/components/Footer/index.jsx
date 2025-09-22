6c5fd45dd1a94776da03a4ee4f3ca60f4410c0cb
Fernanda Dantas <fernandamoreira079@gmail.com>
2025-09-22 16:40:58 -0300

    Add files via upload

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,36 @@
+// src/components/Footer/index.jsx
+
+import './styles.css';
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <div className='footer__wrap'>
+                {/* Informações da empresa */}
+                <div className="footer__section">
+                    <h3> Itaú Unibanco Holding S.A.</h3>
+                    <p>CNPJ: 60.872.504/0001-23</p>
+                    <p>Praça Alfredo Egydio de Souza Aranha, 100, Torre Olavo Setubal, Parque Jabaquara - CEP 04344-902 - São Paulo - Brasil</p>
+                </div>
+
+                <hr className='divider' /> {/* Linha de separação */}
+
+                {/* Contato e Atendimento */}
+                <div className="footer__section">
+                    <h3>Fale com o Itaú</h3>
+                    <p>SAC 0800 728 0728</p>
+                    <p>Ouvidoria 0800 570 0011</p>
+                    <p>Denúncia</p>
+                </div>
+
+                <p className='copy'>&copy; 2025 Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
+// testando
+
+
+
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,32 @@
+// projeto008\src\components\Header\index.jsx
+// src/components/Header/index.jsx
+import { Link, NavLink } from "react-router-dom";
+import './styles.css'; // <-- This is the correct line
+import logo from '../../assets/logo.png'
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header__wrap">
+                <Link to="/" className="brand">
+                    <img src={logo} alt="Logo do site" />
+                    <span>Home</span>
+                </Link>
+                <nav className="nav">
+                    <NavLink to="/servicos" className={({ isActive }) => isActive ? 'link active' : 'link'}>
+                        Serviços
+                    </NavLink>
+                    <NavLink to="/sobre-nos" className={({ isActive }) => isActive ? 'link active' : 'link'}>
+                        Sobre Nós
+                    </NavLink>
+                    <NavLink to="/fale-conosco" className={({ isActive }) => isActive ? 'link active' : 'link'}>
+                        Contato
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
+// testando
